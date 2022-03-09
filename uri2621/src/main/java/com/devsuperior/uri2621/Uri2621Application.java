d6e26f466b5d301fa42529c3979578a36231b252
Hassan <hskuntze@gmail.com>
2022-03-09 16:05:50 -0300

    URI 2621

--- uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2621;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2621.dto.ProductDTO;
+import com.devsuperior.uri2621.projections.ProductProjection;
 import com.devsuperior.uri2621.repositories.ProductRepository;
 
 @SpringBootApplication
@@ -19,6 +24,17 @@ public class Uri2621Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		List<ProductProjection> list = repository.search1(10, 20, "P");
+		List<ProductDTO> result = list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+		for(ProductDTO obj : result) {
+			System.out.println("\n");
+			System.out.println(obj.getName());
+		}
+		System.out.println("\n\n\n");
+		List<ProductDTO> result2 = repository.search2(10, 20, "P");
+		for(ProductDTO obj : result2) {
+			System.out.println("\n");
+			System.out.println(obj.getName());
+		}
 	}
 }
---
+++ uri2621/src/main/java/com/devsuperior/uri2621/dto/ProductDTO.java
@@ -0,0 +1,27 @@
+package com.devsuperior.uri2621.dto;
+
+import com.devsuperior.uri2621.projections.ProductProjection;
+
+public class ProductDTO {
+	
+	private String name;
+	
+	public ProductDTO() {
+	}
+
+	public ProductDTO(String name) {
+		this.name = name;
+	}
+	
+	public ProductDTO(ProductProjection projection) {
+		name = projection.getName();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
