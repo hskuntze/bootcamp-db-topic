f509f1534aa74e2236ed789c26ba163344192954
Hassan <hskuntze@gmail.com>
2022-03-10 15:03:55 -0300

    URI 2609

--- uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2609;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import com.devsuperior.uri2609.repositories.CategoryRepository;
 
 @SpringBootApplication
@@ -19,6 +24,17 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		List<CategorySumProjection> list = repository.search1();
+		List<CategorySumDTO> result = list.stream().map(x -> new CategorySumDTO(x)).collect(Collectors.toList());
+		System.out.println("SQL Raíz\n\n");
+		for(CategorySumDTO obj : result) {
+			System.out.println(obj.getName() + ", " + obj.getSum());
+		}
+		System.out.println("------------------");
+		List<CategorySumDTO> result2 = repository.search2();
+		System.out.println("JPQL\n\n");
+		for(CategorySumDTO obj : result2) {
+			System.out.println(obj.getName() + ", " + obj.getSum());
+		}
 	}
 }
